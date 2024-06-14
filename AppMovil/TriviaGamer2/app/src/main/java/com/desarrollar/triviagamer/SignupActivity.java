0f62c0f85f07b3f55695c2a6a6922e6e18daba49
matiasp23 <matiaspedraza023@gmail.com>
2024-06-13 21:47:17 -0300

    actualizando rama personal

--- AppMovil/TriviaGamer2/app/src/main/java/com/desarrollar/triviagamer/SignupActivity.java
+++ AppMovil/TriviaGamer2/app/src/main/java/com/desarrollar/triviagamer/SignupActivity.java
@@ -3,24 +3,14 @@ package com.desarrollar.triviagamer;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-<<<<<<< HEAD
-import android.view.View;
-=======
->>>>>>> 5b6d0d3df10ddbdb0d016314cbbd1cf02fc67472
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-<<<<<<< HEAD
-
-public class SignupActivity extends AppCompatActivity {
-    EditText email, password, confirmPassword;
-=======
 import java.util.regex.Pattern;
 
 public class SignupActivity extends AppCompatActivity {
     EditText username, password, confirmPassword;
->>>>>>> 5b6d0d3df10ddbdb0d016314cbbd1cf02fc67472
     Button signupButton;
     TextView loginRedirectText;
     DBHelper DB;
@@ -30,54 +20,6 @@ public class SignupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-<<<<<<< HEAD
-        email = (EditText) findViewById(R.id.signup_email);
-        password = (EditText) findViewById(R.id.signup_password);
-        confirmPassword = (EditText) findViewById(R.id.signup_confirm);
-        signupButton = (Button) findViewById(R.id.signup_button);
-        loginRedirectText = (TextView) findViewById(R.id.loginRedirectText);
-        DB = new DBHelper(this);
-
-        signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String userEmail = email.getText().toString();
-                String userPassword = password.getText().toString();
-                String userConfirmPassword = confirmPassword.getText().toString();
-
-                if (userEmail.equals("") || userPassword.equals("") || userConfirmPassword.equals("")) {
-                    Toast.makeText(SignupActivity.this, "Por favor llene todos los campos", Toast.LENGTH_SHORT).show();
-                } else {
-                    if (userPassword.equals(userConfirmPassword)) {
-                        Boolean checkUserEmail = DB.checkEmail(userEmail);
-                        if (!checkUserEmail) {
-                            Boolean insert = DB.insertData(userEmail, userPassword);
-                            if (insert) {
-                                Toast.makeText(SignupActivity.this, "Registro exitoso", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                                startActivity(intent);
-                            } else {
-                                Toast.makeText(SignupActivity.this, "Error en el registro", Toast.LENGTH_SHORT).show();
-                            }
-                        } else {
-                            Toast.makeText(SignupActivity.this, "Usuario ya existente, por favor inicie sesión", Toast.LENGTH_SHORT).show();
-                        }
-                    } else {
-                        Toast.makeText(SignupActivity.this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-        });
-
-        loginRedirectText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-=======
         username = findViewById(R.id.signup_username);
         password = findViewById(R.id.signup_password);
         confirmPassword = findViewById(R.id.signup_confirm);
@@ -153,5 +95,4 @@ public class SignupActivity extends AppCompatActivity {
 
         return numberCount >= 2 && specialCharCount >= 1;
     }
->>>>>>> 5b6d0d3df10ddbdb0d016314cbbd1cf02fc67472
 }
